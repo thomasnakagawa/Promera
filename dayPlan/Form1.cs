c83b04185c424d802c6bc559801a6d92b847215a
= <thomasnakagawa@gmail.com>
2016-03-05 16:11:56 -0800

    Results window opens

--- dayPlan/Form1.cs
+++ dayPlan/Form1.cs
@@ -12,10 +12,14 @@ namespace dayPlan
 {
     public partial class Form1 : Form
     {
+        resultForm myOutput;
         public Form1()
         {
             InitializeComponent();
-            locationBox.
+            locationBox.Enabled = false;
+            groupBox.SelectedIndex = 0;
+            activityBox.SelectedIndex = 0;
+            budgetBox.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,6 +36,12 @@ namespace dayPlan
 
             //no bad inputs, so turn the error label off
             errorLabel.Visible = false;
+            if(myOutput != null && !myOutput.IsDisposed)
+            {
+                myOutput.Dispose();
+            }
+            myOutput = new resultForm("Hello World!");
+            myOutput.Show();
         }
     }
 }
---
+++ dayPlan/resultForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dayPlan
+{
+    public partial class resultForm : Form
+    {
+        public resultForm(String input)
+        {
+            InitializeComponent();
+            richTextBox1.Text = input;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
